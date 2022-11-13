f5e19993f3ecd8a7052255d9a4043f7ecea9510f
lezcano17 <jlezcano17@outlook.com>
2022-11-13 14:36:43 -0300

    Agregado crud example

--- lib/config/routes.dart
+++ lib/config/routes.dart
@@ -5,9 +5,11 @@ import 'package:get/get.dart';
 class Routes {
   static const String _login = "/";
   static const String _home = '/home';
+  static const String _crud = '/home/crud';
 
   static String getLoginRoute() => _login;
   static String getHomeRoute() => _home;
+  static String getCrudRoute() => _crud;
 
   static List<GetPage> routes = [
     GetPage(
--- lib/pages/home/views/home.dart
+++ lib/pages/home/views/home.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:cliinic/pages/common/app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
@@ -21,9 +23,19 @@ class _HomeScreenState extends State<HomeScreen> {
   Post? personas;
   var isLoaded = false;
 
+  List<Map<String, dynamic>> _journals = [];
+
+  bool _isLoading = true;
   void _onTapNavBar(int index) {
     setState(() {
       _selectedIndex = index;
+    });
+  }
+
+  void _refreshJournals() async {
+    /** Load */
+    setState(() {
+      _isLoading = false;
     });
   }
 
@@ -40,14 +52,92 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   void initState() {
     super.initState();
-
+    _refreshJournals();
     get_personas();
+  }
+
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
+
+  void _showForm(int? id) async {
+    if (id != null) {
+      log("FUNCA");
+    }
+
+    showModalBottomSheet(
+        context: context,
+        elevation: 5,
+        isScrollControlled: true,
+        builder: (_) => Container(
+              padding: EdgeInsets.only(
+                top: 15,
+                left: 15,
+                right: 15,
+                // this will prevent the soft keyboard from covering the text fields
+                bottom: MediaQuery.of(context).viewInsets.bottom + 120,
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  TextField(
+                    controller: _titleController,
+                    decoration: const InputDecoration(hintText: 'Title'),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  TextField(
+                    controller: _descriptionController,
+                    decoration: const InputDecoration(hintText: 'Description'),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                    onPressed: () async {
+                      // Save new journal
+                      if (id == null) {
+                        await _addItem();
+                      }
+
+                      if (id != null) {
+                        await _updateItem(id);
+                      }
+
+                      // Clear the text fields
+                      _titleController.text = '';
+                      _descriptionController.text = '';
+
+                      // Close the bottom sheet
+                      Navigator.of(context).pop();
+                    },
+                    child: Text(id == null ? 'Create New' : 'Update'),
+                  )
+                ],
+              ),
+            ));
+  }
+
+  // Insert a new journal to the database
+  Future<void> _addItem() async {
+    /*Create*/
+  }
+
+  // Update an existing journal
+  Future<void> _updateItem(int id) async {
+    /*update*/
+  }
+
+  // Delete an item
+  void _deleteItem(int id) async {
+    /*DELETE*/
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const CustomAppBarHome(),
+      appBar: const CustomAppBar(),
       body: Visibility(
         visible: isLoaded,
         replacement: const Center(
@@ -60,6 +150,8 @@ class _HomeScreenState extends State<HomeScreen> {
           },
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add), onPressed: () => _showForm(0)),
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: Colors.cyan,
         unselectedItemColor: Colors.white,
